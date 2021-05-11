8c61c4cd9cc840fbede3d96a746c1878c6641c2c
pedro.souza <pedro.souza@life.net.br>
2021-05-11 16:47:40 -0300

    Recebendo imagem e numero de telefone na requisição

--- api/src/controllers/Sticker.js
+++ api/src/controllers/Sticker.js
@@ -1,5 +1,10 @@
 const StickerController = {
-  send() {
+  send(req, res) {
+    const { image, phoneNumber } = req.body;
+
+    console.log(image);
+    console.log(phoneNumber);
+
     //recebo uma imagem e um numero
     //chamo o send sticker do venom com o numero a enviar e a imagem
   },
